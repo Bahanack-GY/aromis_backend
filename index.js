03636efbea2f872b5157d3b6226bbc735d445478
LeHavker <georgesyvan12@gmail.com>
2024-10-31 23:13:03 +0100

    Installed mongodg mongoose

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) =>{ res.send('Hello World!')});
 
 mongoose
     .connect(mongoDBURL)
-    .then(() => {console.log("MongoDB connected")})
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+        
+        console.log("MongoDB connected")})
     .catch((err) => {console.log(err)});
