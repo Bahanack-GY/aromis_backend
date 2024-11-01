a44fc3e107d2a423fea5fdef59787efa330ece67
LeHavker <georgesyvan12@gmail.com>
2024-11-01 21:10:32 +0100

    created parfum schema

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 export const PORT = 3000;
-export const mongoDBURL = "mongodb+srv://aromis:C@ssandra12@cluster0.6uhtjk8.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0"
+export const mongoDBURL = "mongodb+srv://root:Cassandra12@cluster0.6uhtjk8.mongodb.net/nodeApi?retryWrites=true&w=majority&appName=Cluster0"
